9978d9eb1fa501ede43a900b99e49244975cc83b
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-10-11 21:50:09 -0400

    setup basic init function and function for first three options

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const db = require("./connection");
 
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,53 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+const viewAll = (table) => {
+    const viewAllQuery = "SELECT * FROM ";
+    db.query(viewAllQuery + table, (err, results) => {
+        if (err) {
+            console.error(err);
+        }
+        console.table(results)
+    });
+}
+
+const initQuestion = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "firstAction",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee"]
+    }
+]
+
+const init = () => {
+    inquirer
+        .prompt(initQuestion)
+            .then((data) => {
+                switch(data.firstAction) {
+                    case "View all departments":
+                        viewAll("department");
+                        break;
+                    case "View all roles":
+                        viewAll("role");
+                        break;
+                    case "View all employees":
+                        viewAll("employee");
+                        break;
+                    case "Add a department":
+
+                        break;
+                    case "Add a role":
+
+                        break;
+                    case "Add an employee":
+
+                        break;
+                }
+            })
+}
+
+init();
 
 app.use((req, res) => {
     res.status(404).end();
